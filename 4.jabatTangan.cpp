4bdd501dd07aaabb67d82b379a60e55587efc61b
ultramankamen <44142812+ultramankamen@users.noreply.github.com>
2018-11-10 13:04:23 +0700

    Add files via upload

---
+++ 4.jabatTangan.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int nilai, sum;
+ cout<<"Masukkan Batas Nilai :";cin>>nilai;
+ 
+ for(int a = 0; a < nilai; a++)
+ {
+  sum += a;
+ 
+ }
+ cout<<"Jumlah jabat tangan: "<<sum<<endl;
+}
